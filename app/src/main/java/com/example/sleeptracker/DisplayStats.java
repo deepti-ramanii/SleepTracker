6b061ea0664a870ee6efa0417c8b29f1e48c7701
Deepti Ramani <deepti_ramani@hotmail.com>
2020-12-04 11:40:31 -0800

    updated displaystats

--- app/src/main/java/com/example/sleeptracker/DisplayStats.java
+++ app/src/main/java/com/example/sleeptracker/DisplayStats.java
@@ -8,20 +8,32 @@ import br.com.sapereaude.maskedEditText.MaskedEditText; // https://www.geeksforg
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class DisplayStats extends AppCompatActivity {
+    private SleepStatsDatabase sleepStatsDatabase;
     private MaskedEditText startDate;
     private MaskedEditText endDate;
-    private Button getDateBounds;
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_display_stats);
+        sleepStatsDatabase = SleepStatsDatabase.getInstance(this);
+
         startDate = this.findViewById(R.id.start_date);
         endDate = this.findViewById(R.id.end_date);
-        getDateBounds = this.findViewById(R.id.get_date_bounds);
     }
 
     public void getStats(View view) {
+        int startDay = Integer.parseInt(startDate.getText().toString().substring(0, 2));
+        int startMonth = Integer.parseInt(startDate.getText().toString().substring(2, 4));
+        int startYear = Integer.parseInt(startDate.getText().toString().substring(4, 8));
 
+        int endDay = Integer.parseInt(endDate.getText().toString().substring(0, 2));
+        int endMonth = Integer.parseInt(endDate.getText().toString().substring(2, 4));
+        int endYear = Integer.parseInt(endDate.getText().toString().substring(4, 8));
+
+        List<SleepStats> sleepStats = sleepStatsDatabase.getBetweenDates(SleepStats.getBeginningOfDay(startDay, startMonth, startYear), SleepStats.getEndOfDay(endDay, endMonth, endYear));
+        //TODO: do stuff with these stats
     }
 }
