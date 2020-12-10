16b3728a0e8f6a055c36adc0df55ff6ec59779b8
Deepti Ramani <deepti_ramani@hotmail.com>
2020-12-09 20:36:31 -0800

    fixed fake data

--- app/src/main/java/com/example/sleeptracker/AddingFakeData.java
+++ app/src/main/java/com/example/sleeptracker/AddingFakeData.java
@@ -10,8 +10,8 @@ public class AddingFakeData {
         SleepStatsDatabase sleepStatsDatabase = SleepStatsDatabase.getInstance(context);
         Random myRandom = new Random();
         for(int i = 1; i <= 9; i++) {
-            Date fakeSleepTime = new Date(2020, 12, i, myRandom.nextInt(4) + 20, myRandom.nextInt(60), myRandom.nextInt(60));
-            Date fakeWakeUpTime = new Date(2020, 12, i + 1, myRandom.nextInt(6) + 5, myRandom.nextInt(60), myRandom.nextInt(60));
+            Date fakeSleepTime = new Date(120, 11, i, myRandom.nextInt(4) + 20, myRandom.nextInt(60), myRandom.nextInt(60));
+            Date fakeWakeUpTime = new Date(120, 11, i + 1, myRandom.nextInt(6) + 5, myRandom.nextInt(60), myRandom.nextInt(60));
             int rating = myRandom.nextInt(11);
             sleepStatsDatabase.insert(fakeSleepTime.getTime(), fakeWakeUpTime.getTime(), rating);
         }
